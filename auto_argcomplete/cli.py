16de5af9840b3dc0664d2d56b746cb7288725e37
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-28 21:58:54 +0900

    Add --quote/--no-quote

--- auto_argcomplete/cli.py
+++ auto_argcomplete/cli.py
@@ -19,10 +19,15 @@ def main():
                         default=True, action='store_true', dest='show_desc')
     parser.add_argument('--no-description', help='not show description',
                         default=True, action='store_false', dest='show_desc')
+    parser.add_argument('--quote', help='with quote',
+                        default=True, action='store_true', dest='with_quote')
+    parser.add_argument('--no-quote', help='not show quote',
+                        default=True, action='store_false', dest='with_quote')
     args = parser.parse_args()
 
     filename = os.path.expanduser(args.filename)
     show_desc = args.show_desc
+    quote = "'" if args.with_quote else ""
 
     # check the file imports argparse
     with open(filename, 'r') as f:
@@ -38,17 +43,20 @@ def main():
     cmd = ['python', filename, '--help']
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
     doc, stderr = proc.communicate()
-    args = parse_defaults(doc, section_name='optional arguments:')
+    opt_args = parse_defaults(doc, section_name='optional arguments:')
 
     opt_descs = []
-    for arg in args:
+    for arg in opt_args:
         if arg.long:
             opt = arg.long
         elif arg.short:
             opt = arg.short
         desc = arg.description
-        if show_desc and desc:
-            opt_descs.append("'{opt}:{desc}'".format(opt=opt, desc=desc))
+        if args.show_desc and desc:
+            opt_descs.append("{quote}{opt}:{desc}{quote}".format(opt=opt,
+                                                                 desc=desc,
+                                                                 quote=quote))
         else:
-            opt_descs.append("'{opt}'".format(opt=opt))
+            opt_descs.append("{quote}{opt}{quote}".format(opt=opt,
+                                                          quote=quote))
     print(' '.join(opt_descs))
--- completion/auto_argcomplete-completion.bash
+++ completion/auto_argcomplete-completion.bash
@@ -1,10 +1,9 @@
 _auto_argcomplete () {
   local files
   if (( COMP_CWORD == 1 )); then
-    files=$(compgen -f -- ${COMP_WORDS[COMP_CWORD]})
-    COMPREPLY=( $(echo $files | xargs -n1 | grep '\.py$') )
+    COMPREPLY=( $(compgen -f -- ${COMP_WORDS[COMP_CWORD]}) )
   else
-    COMPREPLY=( $(auto_argcomplete --no-description ${COMP_WORDS[1]}) )
+    COMPREPLY=( $(auto_argcomplete --no-description --no-quote ${COMP_WORDS[1]}) )
   fi
 }
 complete -F _auto_argcomplete -o filenames python
